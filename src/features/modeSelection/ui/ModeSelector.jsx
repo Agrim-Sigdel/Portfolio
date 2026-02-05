4f42b8340034d359e85286155212d9a7bfeb1335
Agrim-Sigdel <sigdelagrim35@gmail.com>
2026-02-05 19:49:00 +0545

    change title

--- src/features/modeSelection/ui/ModeSelector.jsx
+++ src/features/modeSelection/ui/ModeSelector.jsx
@@ -18,7 +18,7 @@ const ModeSelector = ({ onSelectMode }) => {
                     animate={{ y: 0, opacity: 1 }}
                     transition={{ delay: 0.3, duration: 0.8 }}
                 >
-                    Choose Your Experience
+                   Welcome
                 </motion.h1>
 
                 <motion.p
